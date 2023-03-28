a4bcc301485933ef064e23f33a645b8c6d8a9185
IPIvliev <cccp1945@mail.ru>
2023-03-28 13:38:53 +0300

    first commit

---
+++ demidov_print/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-i=88-2x-u1v4-cj4z5hrr*1yih&@0ow(ghx2-93-u+5gk*204k"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
